2f7203cfa3dd58e6dab522c2cf5a87dec20427a4
Kevin Browne <kbrowne97@gmail.com>
2023-09-22 11:39:31 -0400

    adds get request for bookshelf page

--- client/src/components/Bookshelf/Bookshelf.tsx
+++ client/src/components/Bookshelf/Bookshelf.tsx
@@ -1,11 +1,67 @@
+import axios from "axios";
+import { AuthContext } from "../../contexts/AuthContext";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+type BookType = {
+  id: string;
+  title: string;
+  shelf: string;
+};
+
+type BookshelfType = {
+  wantToRead: BookType[];
+  currentlyReading: BookType[];
+  read: BookType[];
+};
+
 const Bookshelf = () => {
+  const navigate = useNavigate();
+  const { logout, token } = useContext(AuthContext);
+  const [books, setBooks] = useState<BookshelfType>();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+  };
+
+  const getBookshelf = async () => {
+    const request = await axios.request({
+      method: "GET",
+      url: "/api/bookshelf",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    console.log(request.data);
+    setBooks(request.data.books);
+  };
+
+  useEffect(() => {
+    getBookshelf();
+  }, []);
+
   return (
     <>
       <h1>Welcome to bookshelf</h1>
-      <p>
-        jdsg;lkjad;glkajdg lkasdjg a;slkdgj sdlkgjs; kgdj a;sldgkjas gjhsig u
-        asgivuhsv jbdohs sodkjan{" "}
-      </p>
+      <div id="want-to-read">
+        {books?.wantToRead.map((book) => {
+          return <p>{book.title}</p>;
+        })}
+      </div>
+      <div id="currently-reading">
+        {books?.currentlyReading.map((book) => {
+          return <p>{book.title}</p>;
+        })}
+      </div>
+      <div id="read">
+        {books?.read.map((book) => {
+          return <p>{book.title}</p>;
+        })}
+      </div>
+      <button onClick={handleLogout}>logout</button>
     </>
   );
 };
